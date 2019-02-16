c1356fbe77f1e482eb5b03f3aa0c1e2307b006ea
LeslyN <lnombertoc@gmail.com>
2019-02-16 01:41:56 -0500

    Adding card and list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      CardComponent
+      CardComponent,
+      ListComponent
    ],
    imports: [
       BrowserModule,
