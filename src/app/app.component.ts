e11e854b18a40ee506f9a1d7f9f44c6ce3f4cb43
LeslyN <lnombertoc@gmail.com>
2019-02-17 22:43:35 -0500

    Adding dinamic cards and lists with event click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CardComponent } from './card/card.component';
+import { ListComponent } from './list/list.component';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'web-component-app';
+
+  title = 'Prueba Multiplica';
+  
+  cards = [];
+  listCards = [];
+
+  addCard() {
+    this.cards.push(
+      {
+        title: 'Card1',
+        content: 'Contenido card1',
+        imageUrl: 'https://bulma.io/images/placeholders/128x128.png'
+      }
+    )
+  };
+
+
+  addList() {
+    this.listCards.push(
+      {
+        title1: 'Noticia uno',
+        description1: 'Descripción noticia1',
+        logo1: 'https://bulma.io/images/placeholders/48x48.png',
+        title2: 'Noticia dos',
+        description2: 'Descripción noticia2',
+        logo2: 'https://bulma.io/images/placeholders/48x48.png',
+        title3: 'Noticia tres',
+        description3: 'Descripción noticia3',
+        logo3: 'https://bulma.io/images/placeholders/48x48.png'
+      }
+    )
+  }
 }
