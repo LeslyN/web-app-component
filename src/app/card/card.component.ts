7c81d283db509cc81ef75fa7963f137537d4b7fe
LeslyN <lnombertoc@gmail.com>
2019-02-17 22:43:35 -0500

    Adding dinamic cards and lists with event click

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() title;
+  @Input() content;
+  @Input() imageUrl;
+  
+  editing: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  
+  }  
+
+  editCard() {
+    // console.log(event);
+    // let element = event.target;
+    // if (element.files.length > 0) {
+    //   let data = new FormData();
+    //   data.append('file', element.file[0]);
+    // }
+    this.editing = true;
   }
 
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  /** Inyector para noticia uno */ 
+  @Input() title1;
+  @Input() description1;
+  @Input() logo1;
+  
+  /** Inyector para noticia dos */ 
+  @Input() title2;
+  @Input() description2;
+  @Input() logo2;
+  
+  /** Inyector para noticia tres */ 
+  @Input() title3;
+  @Input() description3;
+  @Input() logo3;
 
   constructor() { }
 
